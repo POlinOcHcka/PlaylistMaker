1038f5b52636d95d948747f9564d446ae992bbd2
Polinochka <lrtqzx@gmail.com>
2023-10-03 14:06:14 +0500

    5. homework 9 sprint

--- app/src/main/java/com/practicum/playlistmakerfinish/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmakerfinish/SettingsActivity.kt
@@ -2,9 +2,11 @@ package com.practicum.playlistmakerfinish
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.LinearLayout
 
 class SettingsActivity() : AppCompatActivity() {
 
@@ -18,6 +20,39 @@ class SettingsActivity() : AppCompatActivity() {
         back.setOnClickListener {
             val backIntent = Intent(this, MainActivity::class.java)
             startActivity(backIntent)
+        }
+
+        val share = findViewById<LinearLayout>(R.id.share_the_app)
+
+        share.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            val settingsLinkCourse = getString(R.string.settings_link_course)
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, settingsLinkCourse)
+            startActivity(Intent.createChooser(shareIntent, "Поделиться приложением"))
+        }
+
+        val support = findViewById<LinearLayout>(R.id.write_to_support)
+
+        support.setOnClickListener {
+            val supportEmail = getString(R.string.settings_support_email)
+            val subject = getString(R.string.settings_subject)
+            val message = getString(R.string.settings_message)
+
+            val emailIntent = Intent(Intent.ACTION_SENDTO)
+            emailIntent.data = Uri.parse("mailto:")
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, supportEmail)
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            emailIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(emailIntent)
+        }
+
+        val agreement = findViewById<LinearLayout>(R.id.user_agreement)
+
+        agreement.setOnClickListener {
+            val url = getString(R.string.settings_link_offer)
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(browserIntent)
         }
     }
 
